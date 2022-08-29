099f671aba200a93b89a75be8144a1ec9ce4969a
RahulSharma4329 <rahulsharma4329@gmail.com>
2022-08-29 08:42:20 +0530

    heh

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require("express")
-require("dotenv/config")
-const mongoose = require("mongoose")
-const cred = require("./models/cred")
+const express = require("express");
+require("dotenv/config");
+const mongoose = require("mongoose");
+const cred = require("./models/cred");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-const app = express()
-mongoose.connect(process.env.DB_CONNECTION, () => {
-
-})
+const app = express();
+mongoose.connect(process.env.DB_CONNECTION, () => {});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
@@ -15,44 +14,45 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 router = express.Router();
 
-app.get("/", (req,res) =>{
-    res.send("HMS_API is live");
-})
+app.get("/", (req, res) => {
+  res.send("HMS_API is live");
+});
 
-app.post("/register", async (req,res) => {
-    try{
-        const {username} = req.body
-        const {password} = req.body
-        
-        const senddata = new cred({
-            username: username,
-            password: password
-        })
+app.post("/register", async (req, res) => {
+  try {
+    const { username } = req.body;
+    const { password } = req.body;
 
-        const saveCred = await senddata.save()
-        res.status(200).json({success: true, data: saveCred})
-    }
-    catch (error1){
-        res.status(400).json({success:false,data: [],error:error1})
-    }
-})
+    const senddata = new cred({
+      username: username,
+      password: password,
+    });
 
-app.post("/login",async (req,res) => {
-    try{
-        const recdata = await cred.find({username: req.body.username,password:req.body.password})
-        res.status(200).json({success: true, data: recdata})
-    }
-    catch(error){
-        res.status(400).json({success:false,data:[],error:error})
-    }
-})
+    const saveCred = await senddata.save();
+    res.status(200).json({ success: true, data: saveCred });
+  } catch (error1) {
+    res.status(400).json({ success: false, data: [], error: error1 });
+  }
+});
 
+app.post("/login", async (req, res) => {
+  try {
+    const recdata = await cred.find({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    res.status(200).json({ success: true, data: recdata });
+  } catch (error) {
+    res.status(400).json({ success: false, data: [], error: error });
+  }
+});
 
-app.listen(port,() => {
-    console.log("Server running on port 5000....")
-})
+app.listen(port, () => {
+  console.log("Server running on port 5000....");
+});
