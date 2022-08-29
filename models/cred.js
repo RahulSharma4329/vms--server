ec9f24f58c5fcb60f0e2cb8a05b3111fa95590d4
RahulSharma4329 <rahulsharma4329@gmail.com>
2022-08-29 09:13:14 +0530

    heh

--- models/cred.js
+++ models/cred.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
-
 const Cred = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true
-    }
-
-})
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model("cred", Cred);
